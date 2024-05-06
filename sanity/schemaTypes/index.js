98ec6bd8686274c1123f97e35ec0c5fe71c49077
tylerroliverr <tyler.oliverjames@gmail.com>
2024-05-06 12:28:29 +1000

    Added schema types to schema index

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,3 +1,7 @@
+import EducationSchema from "./EducationSchema";
+import ExperienceSchema from "./ExperienceSchema";
 import ProjectsSchema from "./ProjectsSchema";
+import ReferenceSchema from "./ReferenceSchema";
+import SkillSchema from "./SkillSchema";
 
-export const schemaTypes = [ProjectsSchema]
+export const schemaTypes = [ProjectsSchema, ExperienceSchema, EducationSchema, SkillSchema, ReferenceSchema]
